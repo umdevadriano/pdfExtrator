5d0822ba7d56869230805c5844e4f34ac41f0dcc
adrianodisam <adrianodisam@gmail.com>
2024-10-05 16:52:59 -0300

    correção com envio de mias de um arquivo

--- src/app.js
+++ src/app.js
@@ -1,48 +1,51 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
-const  { extractTextFromPDF, findLinesWithWords ,transformStringsToArray}  = require('./controllers/textoExtrator');
-var nomeArquivo = ''
+const {findLinesWithWords, transformStringsToArray } = require('./controllers/textoExtrator');
 
 // Configuração do multer para armazenamento de arquivos
 const storage = multer.memoryStorage(); // Armazena o arquivo em memória
 const upload = multer({ storage: storage });
 const pdf = require('pdf-parse');
-var caminho = ''
 
-// Rota para receber o arquivo
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.array('file', 10), async (req, res) => {
+  const uploadedFiles = req.files;
 
-  if (!req.file ) {
+  if (!uploadedFiles || uploadedFiles.length === 0) {
     return res.status(400).send('Nenhum arquivo foi enviado.');
   }
-  if (!req.body.texto ) {
-    return res.status(400).send('Nenhum texto a procurar');
+  if (!req.body.texto) {
+    return res.status(400).send('Nenhum texto a procurar.');
   }
-  const nomeArquivo = req.file.originalname
-  const fileBuffer = req.file.buffer;
-  const pdfData = pdf(fileBuffer);
-  const palavras = transformStringsToArray(req.body.texto.split(','))
-  //pega o pdf armazenado em memoria e faaz tratativa de encontrar o texto e devolver
-  pdfData.then((data) => {
-    try {
-      const texto = data.text;
-      const encontrado = findLinesWithWords(texto,palavras)
 
-      res.json(
-                  { paginas :data.numpages,
-                    info: data.info,
-                    texto: data.text,
-                    palavras: encontrado,
-                    arquivo: nomeArquivo,
-  
-                }) 
+  const palavras = transformStringsToArray(req.body.texto.split(','));
+  const arrayInformacoes = [];
+
+  for (const file of uploadedFiles) {
+    try {
+      const nomeArquivo = file.originalname;
+      const fileBuffer = file.buffer; // Use 'buffer' em vez de 'file' para obter o conteúdo do arquivo
+      const pdfData = await pdf(fileBuffer);
+      const texto = pdfData.text;
+      const encontrado = findLinesWithWords(texto, palavras);
+
+      const info = {
+        paginas: pdfData.numpages,
+        info: pdfData.info,
+        texto: pdfData.text,
+        palavras: encontrado,
+        arquivo: nomeArquivo,
+      };
+
+      arrayInformacoes.push(info);
     } catch (error) {
       console.error('Erro ao extrair texto do PDF:', error);
     }
-  });
+  }
+
+  res.json(arrayInformacoes);
 });
-    
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
